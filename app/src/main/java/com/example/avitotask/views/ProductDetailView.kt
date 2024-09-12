d4dad6ac031bd28ce786ebc60e1815fbfe8ab24a
Коломыцев Андрей <akolomytsev@softrust.ru>
2024-09-12 04:52:42 +0300

    Рефакторинг View

--- app/src/main/java/com/example/avitotask/views/ProductDetailView.kt
+++ app/src/main/java/com/example/avitotask/views/ProductDetailView.kt
@@ -1,5 +1,6 @@
 package com.example.avitotask.views
 
+import android.content.Context
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -33,20 +35,23 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.avitotask.R
+import com.example.avitotask.retrofit.Product
 import com.example.avitotask.viewModels.ProductDetailViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductDetailView(productId: String, onBackClick: () -> Unit) {
     val productDetailVM: ProductDetailViewModel = hiltViewModel()
-    LaunchedEffect(Unit) {
-        productDetailVM.getProductById(productId)
-    }
+
     val product = productDetailVM.product.value
     val pagerState = rememberPagerState(pageCount = { product?.images?.size ?: 0 })
     val context = LocalContext.current;
     val scrollState = rememberScrollState()
     val isLoading = productDetailVM.isLoading.value
+
+    LaunchedEffect(Unit) {
+        productDetailVM.getProductById(productId)
+    }
 
     Column(
         modifier = Modifier
@@ -64,6 +69,7 @@ fun ProductDetailView(productId: String, onBackClick: () -> Unit) {
             },
             colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Transparent)
         )
+
         if (isLoading) IsLoading()
         else if (productDetailVM.errorMessage.value != null) {
             ErrorScreen(
@@ -79,35 +85,7 @@ fun ProductDetailView(productId: String, onBackClick: () -> Unit) {
             ) {
                 if (product != null) {
 
-                    Box(modifier = Modifier.fillMaxWidth()) {
-                        HorizontalPager(
-                            state = pagerState,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(350.dp),
-                            pageSpacing = 16.dp,
-
-                            ) { page ->
-                            ProductImage(
-                                images = product.images,
-                                index = page,
-                                modifier = Modifier
-                                    .fillMaxSize(),
-                                context
-                            )
-                        }
-                        if (pagerState.pageCount > 1)
-                            Text(
-                                text = " ${pagerState.currentPage + 1}-${product.images.size} ",
-                                color = Color.White,
-                                fontSize = 14.sp,
-                                fontWeight = FontWeight.Bold,
-                                modifier = Modifier
-                                    .align(Alignment.BottomCenter)
-                                    .padding(8.dp)
-                                    .background(Color.Gray)
-                            )
-                    }
+                    ImageSlider(pagerState, product, context)
 
                     Spacer(modifier = Modifier.height(16.dp))
 
@@ -133,39 +111,7 @@ fun ProductDetailView(productId: String, onBackClick: () -> Unit) {
                     )
                     Spacer(modifier = Modifier.height(4.dp))
 
-                    Column {
-                        val specsToShow = product.product_specifications
-                        if (specsToShow.isNotEmpty()) {
-                            specsToShow.forEach { specification ->
-                                Text(
-                                    text = buildAnnotatedString {
-                                        if (specification.key != null) {
-                                            withStyle(
-                                                style = SpanStyle(
-                                                    color = Color.Gray,
-                                                    fontWeight = FontWeight.Bold
-                                                )
-                                            ) {
-                                                append("${specification.key}: ")
-                                            }
-                                        }
-                                        withStyle(style = SpanStyle(color = Color.White)) {
-                                            append(specification.value)
-                                        }
-                                    },
-                                    fontSize = 12.sp,
-                                    modifier = Modifier.padding(vertical = 2.dp)
-                                )
-                            }
-                        } else {
-                            Text(
-                                text = "Нет информации о характеристиках",
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Normal,
-                                modifier = Modifier.padding(top = 8.dp)
-                            )
-                        }
-                    }
+                    Characteristics(product)
 
                     Spacer(modifier = Modifier.height(16.dp))
 
@@ -183,5 +129,79 @@ fun ProductDetailView(productId: String, onBackClick: () -> Unit) {
                 }
             }
         }
+    }
+}
+
+@Composable
+private fun Characteristics(product: Product) {
+    Column {
+        val specsToShow = product.product_specifications
+        if (specsToShow.isNotEmpty()) {
+            specsToShow.forEach { specification ->
+                Text(
+                    text = buildAnnotatedString {
+                        if (specification.key != null) {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Gray,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            ) {
+                                append("${specification.key}: ")
+                            }
+                        }
+                        withStyle(style = SpanStyle(color = Color.White)) {
+                            append(specification.value)
+                        }
+                    },
+                    fontSize = 12.sp,
+                    modifier = Modifier.padding(vertical = 2.dp)
+                )
+            }
+        } else {
+            Text(
+                text = "Нет информации о характеристиках",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+    }
+}
+
+@Composable
+private fun ImageSlider(
+    pagerState: PagerState,
+    product: Product,
+    context: Context
+) {
+    Box(modifier = Modifier.fillMaxWidth()) {
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(350.dp),
+            pageSpacing = 16.dp,
+
+            ) { page ->
+            ProductImage(
+                images = product.images,
+                index = page,
+                modifier = Modifier
+                    .fillMaxSize(),
+                context
+            )
+        }
+        if (pagerState.pageCount > 1)
+            Text(
+                text = " ${pagerState.currentPage + 1}-${product.images.size} ",
+                color = Color.White,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(8.dp)
+                    .background(Color.Gray)
+            )
     }
 }
